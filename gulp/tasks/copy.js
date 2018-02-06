245ee5603f0d2dda31670fab5a15f9375288d7fa
Anton Shulzhenko <shulzhenkomailbox@gmail.com>
2018-02-06 18:35:22 +0200

    added image minification task

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,5 +1,6 @@
-const gulp   = require('gulp');
-const config = require('../config.js');
+const gulp     = require('gulp');
+const config   = require('../config.js');
+const imagemin = require('gulp-imagemin');
 
 gulp.task('copy:fonts', () => {
   return gulp
@@ -11,8 +12,12 @@ gulp.task('copy:img', () => {
   return gulp
     .src([
       `${config.src.img}/**/*.{jpg,png,jpeg,svg,gif}`,
+      `${config.src.icons}/**/*.{png,svg}`,
       `!${config.src.img}/svgo/**/*.*`
     ])
+    .pipe(imagemin({
+      progressive: true
+    }))
     .pipe(gulp.dest(config.dest.img));
 });
 
